9e513708b518be0f29abf286bf903710e93c25ea
Utpalkant Sahu <89702252+utpal3000@users.noreply.github.com>
2023-11-30 06:16:31 +0000

    day2.1

---
+++ inputs.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter some input:");
+        int roll=input.nextInt();
+        System.out.println("Roll is " + roll);
+
+    }
+} 
---
+++ primitives.java
@@ -0,0 +1,15 @@
+public class primitives {
+    public static void main(String[] args) {
+        int roll = 34;
+        double account_no = 343243.2342525;
+        float pi=3.14f;
+        long girafee=7846465789956454L;
+        char abc = 'c';
+        boolean checck =true;
+        // all above are primitive data types.
+        
+
+
+
+    }
+}
